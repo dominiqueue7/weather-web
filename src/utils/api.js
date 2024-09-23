da67425b86a89242f4c78af5ab24ee6c18cdaec7
im donghyeon <dominiqueue7@gmail.com>
2024-09-23 17:03:20 +0900

    for use SSR

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,8 @@
-import { API_KEY, getFormattedDate, getCurrentHourBaseTime, getShortTermForecastBaseTime, getDailyForecastBaseTime } from './helpers';
-
-const BASE_URL = '/api';
+import { getFormattedDate, getCurrentHourBaseTime, getShortTermForecastBaseTime, getDailyForecastBaseTime } from './helpers';
 
 export const fetchCurrentWeather = async () => {
   const baseTime = getCurrentHourBaseTime();
-  const response = await fetch(`${BASE_URL}/getUltraSrtNcst?serviceKey=${API_KEY}&numOfRows=10&pageNo=1&dataType=JSON&base_date=${getFormattedDate()}&base_time=${baseTime}&nx=55&ny=127`);
+  const response = await fetch(`/forecast/api/current/&numOfRows=10&pageNo=1&dataType=JSON&base_date=${getFormattedDate()}&base_time=${baseTime}&nx=55&ny=127`);
   const data = await response.json();
   if (data.response && data.response.body && data.response.body.items) {
     return data.response.body.items.item;
@@ -15,7 +13,7 @@ export const fetchCurrentWeather = async () => {
 
 export const fetchShortTermForecast = async () => {
   const baseTime = getShortTermForecastBaseTime();
-  const response = await fetch(`${BASE_URL}/getUltraSrtFcst?serviceKey=${API_KEY}&numOfRows=60&pageNo=1&dataType=JSON&base_date=${getFormattedDate()}&base_time=${baseTime}&nx=55&ny=127`);
+  const response = await fetch(`/forecast/api/short-term/&numOfRows=60&pageNo=1&dataType=JSON&base_date=${getFormattedDate()}&base_time=${baseTime}&nx=55&ny=127`);
   const data = await response.json();
   if (data.response && data.response.body && data.response.body.items) {
     return data.response.body.items.item;
@@ -26,7 +24,7 @@ export const fetchShortTermForecast = async () => {
 
 export const fetchDailyForecast = async () => {
   const baseTime = getDailyForecastBaseTime();
-  const response = await fetch(`${BASE_URL}/getVilageFcst?serviceKey=${API_KEY}&numOfRows=1000&pageNo=1&dataType=JSON&base_date=${getFormattedDate()}&base_time=${baseTime}&nx=55&ny=127`);
+  const response = await fetch(`/forecast/api/daily/&numOfRows=1000&pageNo=1&dataType=JSON&base_date=${getFormattedDate()}&base_time=${baseTime}&nx=55&ny=127`);
   const data = await response.json();
   if (data.response && data.response.body && data.response.body.items) {
     return data.response.body.items.item;
